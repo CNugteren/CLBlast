ede78fe49943312ca44275ba3cdf4cfbae501ed0
CNugteren <web@cedricnugteren.nl>
2015-06-29 20:39:51 +0200

    Fixed the license for the correctness testers

--- test/correctness/routines/xaxpy.cc
+++ test/correctness/routines/xaxpy.cc
@@ -1,6 +1,6 @@
 
 // =================================================================================================
-// This file is part of the CLBlast project. The project is licensed under the MIT license. This
+// This file is part of the CLBlast project. The project is licensed under Apache Version 2.0. This
 // project loosely follows the Google C++ styleguide and uses a tab-size of two spaces and a max-
 // width of 100 characters per line.
 //
