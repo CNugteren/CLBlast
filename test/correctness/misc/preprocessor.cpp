426406668e6ebf2ed3a2d3e41e3c97ae5a8fb81d
Cedric Nugteren <web@cedricnugteren.nl>
2017-11-28 20:52:47 +0100

    Improved the pre-processor tester, added GEMV and GER kernels

--- test/correctness/misc/preprocessor.cpp
+++ test/correctness/misc/preprocessor.cpp
@@ -24,24 +24,11 @@
 namespace clblast {
 // =================================================================================================
 
-size_t RunPreprocessor(int argc, char *argv[], const bool silent,
-                       const std::string &kernel_name, const std::string &kernel_source,
-                       const Precision precision) {
-  auto errors = size_t{0};
-  auto passed = size_t{0};
+bool TestKernel(const Device& device, const Context& context,
+                const std::string &kernel_name, const std::string &kernel_source,
+                const Precision precision) {
+
   fprintf(stdout, "* Testing simple OpenCL pre-processor for '%s'\n", kernel_name.c_str());
-
-  // Retrieves the arguments
-  auto help = std::string{"Options given/available:\n"};
-  auto arguments = RetrieveCommandLineArguments(argc, argv);
-  const auto platform_id = GetArgument(arguments, help, kArgPlatform, ConvertArgument(std::getenv("CLBLAST_PLATFORM"), size_t{0}));
-  const auto device_id = GetArgument(arguments, help, kArgDevice, ConvertArgument(std::getenv("CLBLAST_DEVICE"), size_t{0}));
-  if (!silent) { fprintf(stdout, "\n* %s\n", help.c_str()); }
-
-  // Initializes OpenCL
-  const auto platform = Platform(platform_id);
-  const auto device = Device(platform, device_id);
-  const auto context = Context(device);
 
   // Verifies that the current kernel compiles properly (assumes so, otherwise throws an error)
   auto compiler_options_ref = std::vector<std::string>();
@@ -56,13 +43,60 @@ size_t RunPreprocessor(int argc, char *argv[], const bool silent,
     auto compiler_options = std::vector<std::string>();
     const auto program = CompileFromSource(processed_source, precision, kernel_name,
                                            device, context, compiler_options);
-    passed++;
+    return true;
   } catch (...) {
     fprintf(stdout, "* ERROR: Compilation warnings/errors with pre-processed kernel\n");
-    errors++;
+    return false;
   }
+}
+
+// =================================================================================================
+
+size_t RunPreprocessor(int argc, char *argv[], const bool silent,
+                       const Precision precision) {
+  auto errors = size_t{0};
+  auto passed = size_t{0};
+
+  // Retrieves the arguments
+  auto help = std::string{"Options given/available:\n"};
+  auto arguments = RetrieveCommandLineArguments(argc, argv);
+  const auto platform_id = GetArgument(arguments, help, kArgPlatform, ConvertArgument(std::getenv("CLBLAST_PLATFORM"), size_t{0}));
+  const auto device_id = GetArgument(arguments, help, kArgDevice, ConvertArgument(std::getenv("CLBLAST_DEVICE"), size_t{0}));
+  if (!silent) { fprintf(stdout, "\n* %s\n", help.c_str()); }
+
+  // Initializes OpenCL
+  const auto platform = Platform(platform_id);
+  const auto device = Device(platform, device_id);
+  const auto context = Context(device);
+
+  // XAXPY
+  const auto xaxpy_sources =
+    "#define WPT 2\n"
+    #include "../src/kernels/level1/level1.opencl"
+    #include "../src/kernels/level1/xaxpy.opencl"
+  ;
+  if (TestKernel(device, context, "XaxpyFastest", xaxpy_sources, precision)) { passed++; } else { errors++; }
+
+  // XGER
+  const auto xger_sources =
+    "#define WPT 2\n"
+    #include "../src/kernels/level2/level2.opencl"
+    #include "../src/kernels/level2/xger.opencl"
+  ;
+  if (TestKernel(device, context, "Xger", xger_sources, precision)) { passed++; } else { errors++; }
+
+  // XGEMV
+  const auto xgemv_sources =
+    "#define WPT1 2\n"
+    "#define WPT2 2\n"
+    "#define WPT3 2\n"
+    #include "../src/kernels/level2/xgemv.opencl"
+    #include "../src/kernels/level2/xgemv_fast.opencl"
+  ;
+  if (TestKernel(device, context, "XgemvFast", xgemv_sources, precision)) { passed++; } else { errors++; }
 
   // Prints and returns the statistics
+  std::cout << std::endl;
   std::cout << "    " << passed << " test(s) passed" << std::endl;
   std::cout << "    " << errors << " test(s) failed" << std::endl;
   std::cout << std::endl;
@@ -75,12 +109,7 @@ size_t RunPreprocessor(int argc, char *argv[], const bool silent,
 // Main function (not within the clblast namespace)
 int main(int argc, char *argv[]) {
   auto errors = size_t{0};
-  const auto xaxpy_sources =
-  #include "../src/kernels/level1/level1.opencl"
-  #include "../src/kernels/level1/xaxpy.opencl"
-      ;
-  errors += clblast::RunPreprocessor(argc, argv, false,
-                                     "XaxpyFastest", xaxpy_sources, clblast::Precision::kSingle);
+  errors += clblast::RunPreprocessor(argc, argv, false, clblast::Precision::kSingle);
   if (errors > 0) { return 1; } else { return 0; }
 }
 
