ab1c7af03efc66a59b3452628a58b33326ec5fac
Cedric Nugteren <web@cedricnugteren.nl>
2023-07-09 10:36:41 +0200

    Bump to v1.6.1

--- include/clblast.h
+++ include/clblast.h
@@ -39,10 +39,10 @@
   #define PUBLIC_API
 #endif
 
-// Version numbering (v1.6.0)
+// Version numbering (v1.6.1)
 #define CLBLAST_VERSION_MAJOR 1
 #define CLBLAST_VERSION_MINOR 6
-#define CLBLAST_VERSION_PATCH 0
+#define CLBLAST_VERSION_PATCH 1
 
 namespace clblast {
 // =================================================================================================
