07e14a46d13ea4a497362557a7324aa229ba6f50
EJain-Dev <ekansh.jain2011@gmail.com>
2025-09-02 21:09:01 -0400

    Removed android.hpp from include because it isn't supposed to be included explicitly.

--- src/utilities/utilities.hpp
+++ src/utilities/utilities.hpp
@@ -22,7 +22,6 @@
 #include <vector>
 
 #include "clblast.h"
-#include "utilities/android.hpp"  // IWYU pragma: export
 #include "utilities/backend.hpp"
 #include "utilities/msvc.hpp"  // IWYU pragma: export
 
